b9ae0eb8e33091346efe984d99e99746feb04f3e
MaciejKlis <maciej.klis@thecoders.pl>
2020-06-05 18:37:59 +0200

    Array methods - entry point to exercise

---
+++ array-methods/index.js
@@ -0,0 +1,24 @@
+// Write array methods by yourself :)
+
+// .map
+// .filter
+// .reduce
+// .reduceRight
+// .every
+// .some
+// .entries
+
+
+function mapFn(array, callback){}
+
+function filterFn(array, callback){}
+
+function reduceFn(array, callback, initial){}
+
+function reduceRightFn(array, callback, initial){}
+
+function everyFn(array, callback){}
+
+function someFn(array, callback){}
+
+function entriesFn(array){}
