620a5d543ff938969ea3648c440d7ea0fac5124f
MaciejKlis <maciej.klis@thecoders.pl>
2020-06-07 15:06:18 +0200

    Array methodas - added: Map Function & Filter Function

--- array-methods/index.js
+++ array-methods/index.js
@@ -1,4 +1,4 @@
-// Write array methods by yourself :)
+// Write array methods by yourself
 
 // .map
 // .filter
@@ -8,12 +8,45 @@
 // .some
 // .entries
 
+/* Map */
+function mapFn(array, mapCallback) {
+    array.forEach( (element, i) => {
+        array[i] = mapCallback(element);
+    })
+    return array;
+}
 
-function mapFn(array, callback){}
+function mapCallback(element) {
+    return false;
+}
 
-function filterFn(array, callback){}
+// Map example //
+const mapArray = [1,2,3,4,5];
+const mapedArray = mapFn(mapArray, mapCallback);
+//console.log(mapedArray);
 
-function reduceFn(array, callback, initial){}
+/* Filter */
+function filterFn(array, filterCallback) {
+    const resultArray = [];
+    array.forEach( element => {
+
+        if( filterCallback(element) === true )
+            resultArray.push(element);
+
+    })
+
+    return resultArray;
+}
+
+function filterCallback(element){
+    return element >= 10;
+}
+// Filter example //
+const filterArray = [12, 5, 8, 130, 44];
+const filtredArray = filterFn(filterArray, filterCallback);
+// console.log(fitredArray)
+
+function reduceFn(array, callback, initial) {}
 
 function reduceRightFn(array, callback, initial){}
 
